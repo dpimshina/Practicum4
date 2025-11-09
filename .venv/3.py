a16224a016ce9ac3303517d27aa0f74b204d371e
dpimshina <d.pimshina@g.nsu.ru>
2025-11-09 20:08:22 +0700

    3.py

--- .venv/3.py
+++ .venv/3.py
@@ -1,7 +1,6 @@
-print('Как зовут главного персонажа романов Яна Флеминга о вымышленном шпионе секретной разведывательной службы Великобритании?')
-character_name = input('Имя персонажа: ')
-answer = character_name.lower()
-if 'Уильям Сомерсет Моэм' in answer or '007' in answer or 'бонд' in answer:
+print('Как зовут главного персонажа?')
+name = input('Имя персонажа: ')
+if 'Уильям Сомерсет Моэм' in name or '007' in name or 'бонд' in name:
     print('Верно')
 else:
     print('Неверно')
