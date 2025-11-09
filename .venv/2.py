176f06d44b9f27a932cef054877cb1bc2bf1b800
dpimshina <d.pimshina@g.nsu.ru>
2025-11-09 20:05:31 +0700

    2.py

--- .venv/2.py
+++ .venv/2.py
@@ -1,7 +1,7 @@
 print('Какие два слова передал первой радиограммой Александр Попов?')
 first_word = input('Первое слово: ')
 second_word = input('Второе слово: ')
-if first_word.lower() == 'генрих' and second_word.lower() == 'герц':
+if first_word == 'генрих' and second_word == 'герц':
     print('Верно')
 else:
     print('Неверно')
