f4000bd5596bd2ac65455deef44ad48d48cf5cf5
dpimshina <d.pimshina@g.nsu.ru>
2025-11-09 20:12:41 +0700

    Update 6.py

--- .venv/6.py
+++ .venv/6.py
@@ -1,8 +1,7 @@
-score = input("Введите счет матча (например, 3:2): ")
-team1_score, team2_score = map(int, score.split(':'))
-if team1_score > team2_score:
+n1, n2 = map(int, input("Введите счет матча (например, 3:2): ").split(':'))
+if n1 > n2:
     print(1)
-elif team2_score > team1_score:
+elif n2 > n1:
     print(2)
 else:
     print(0)
