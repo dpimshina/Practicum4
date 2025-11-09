9880cd764495ec8d0f8aa7c2d7643a47e96cae0b
dpimshina <d.pimshina@g.nsu.ru>
2025-11-09 20:16:21 +0700

    7.py

--- .venv/7.py
+++ .venv/7.py
@@ -1,8 +1,7 @@
-scores = input("Введите рекорды Кирилла, Арины и Сергея через пробел: ")
-score1, score2, score3 = map(int, scores.split())
-best_score = score1
-if score2 > best_score:
-    best_score = score2
-if score3 > best_score:
-    best_score = score3
+n1, n2, n3 = map(int, input("Введите рекорды Кирилла, Арины и Сергея через пробел: ").split())
+best_score = n1
+if n2 > best_score:
+    best_score = n2
+if n3 > best_score:
+    best_score = n3
 print(best_score)
